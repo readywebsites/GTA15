fc73713831d94110d6526255e343f3e67dc22b9d
readywebsites <biz499websites@gmail.com>
2025-06-30 14:08:40 +0530

    git_backend to git_project

--- gta_project/urls.py
+++ gta_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for gta_backend project.
+URL configuration for gta_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- gta_project/wsgi.py
+++ gta_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gta_backend project.
+WSGI config for gta_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gta_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gta_project.settings')
 
 application = get_wsgi_application()
